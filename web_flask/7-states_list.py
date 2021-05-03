9406dfaa55eac34a609af52394bac6b9f0727414
Matias Zappino <matiaszappino31@gmail.com>
2021-05-03 01:55:07 +0000

    Finished Ex 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
-from flask import Flask
+"""Script that starts a Flask web application"""
 from models import storage
+from flask import Flask
 from flask import render_template
-
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
-def stateslist(state):
+def states_list():
     """Displays a HTML page"""
-    states_list = storage.all("State")
-    return render_template("7-states_list.html", states_list)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
 
 @app.teardown_appcontext
-def teardown_close():
-    """Calls close method"""
+def teardown(exc):
+    """Closing session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0")
